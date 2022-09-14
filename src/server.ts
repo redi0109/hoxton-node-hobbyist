4fd2dd15a17ecfe9a6876c88641c42f5ef536404
Demir Boraj <demirboraj97@gmail.com>
2022-09-14 02:08:23 +0200

    1

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,15 @@ import PrismaClient from "@prisma/client";
 import express from "express";
 import cors from "cors";
 
-const prisma = new PrismaClient.PrismaClient();
+const prisma = new PrismaClient();
 const app = express();
-
-const port = 4000;
-
 app.use(cors());
 app.use(express.json());
+const port = 4000;
+
+
+
+
 
 //users
 app.get("/users", async (req, res) => {
@@ -30,22 +32,52 @@ app.get("/users/:id", async (req, res) => {
 });
 
 app.post("/users", async (req, res) => {
-  const user = await prisma.user.create({
-    data: req.body,
-    include: { hobbies: true },
-  });
-  res.send(user);
-});
+    const addUser = await prisma.user.create({
+        data: {
+            fullName: req.body.name,
+            photo: req.body.photo,
+            email: req.body.email,
+            hobbies: {
+                // @ts-ignore
+                connectOrCreate: req.body.hobbies.map(hobby => ({
+                    where: { name: hobby },
+                    create: { name: hobby }
+                })
+                )
+            }
+        },
+        include: { hobbies: true }
+    })
+    res.send(addUser)
+})
+
 
 app.patch("/users/:id", async (req, res) => {
-  const id = Number(req.params.id);
-  const user = await prisma.user.update({
-    where: { id },
-    data: req.body,
-    include: { hobbies: true },
-  });
-  res.send(user);
-});
+    const id = Number(req.params.id)
+    const user = await prisma.user.update({
+        where: { id: id },
+        data: {
+            fullName: req.body.name,
+            photo: req.body.photo,
+            email: req.body.email,
+            hobbies: {
+                // @ts-ignore
+                connectOrCreate: req.body.hobbies.map(hobby => ({
+                    where: { name: hobby },
+                    create: { name: hobby }
+                })
+                )
+            }
+        },
+        include: { hobbies: true }
+    })
+    if (user) {
+        res.send(user)
+    }
+    else {
+        res.status(404).send({ error: "User not found." })
+    }
+})
 
 app.delete("/users/:id", async (req, res) => {
   const id = Number(req.params.id);
